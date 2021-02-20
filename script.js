8f0c4cfa132d88520fa39cafbafbf59338666dd1
narradorww <rodrigo.anst@gmail.com>
2021-02-20 14:03:57 -0300

    implementada a função da cobrinha se alimentar e crescer

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ snake[0]={
     y: 8*box
             }
 let direction="right";
+let food ={
+    x: Math.floor(Math.random()*15+1)*box,
+    y: Math.floor(Math.random()*15+1)*box
+}
+
+
 
 function criarBG()  {
     context.fillStyle ="lightgreen";
@@ -19,6 +25,12 @@ for(i=0; i < snake.length; i++){
         context.fillRect(snake[i].x, snake[i].y, box, box);
                                   }
 }
+
+function drawFood(){
+    context.fillStyle ='red';
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown',update);
 
 function update(event){
@@ -39,6 +51,7 @@ function iniciarJogo(){
 
         criarBG();
         criarCobrinha();
+        drawFood();
 
         let snakeX= snake[0].x;
         let snakeY= snake[0].y;
@@ -48,7 +61,14 @@ function iniciarJogo(){
         if(direction=="up") snakeY-=box;
         if(direction=="down") snakeY+=box;
 
-        snake.pop();
+        if (snakeX != food.x || snakeY != food.y){
+            snake.pop();
+        }
+        else{
+            food.x= Math.floor(Math.random()*15+1)*box;
+            food.y=Math.floor(Math.random()*15+1)*box;
+        }
+        
 
         let newHead = {
             x: snakeX,
